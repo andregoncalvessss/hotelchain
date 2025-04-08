8e154d1197fbf1fd4e239fb8959cd254e09ea590
andregoncalves <andrefilipegoncalves@ipvc.pt>
2025-04-08 17:47:35 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'hotelchain',
+  webDir: 'www'
+};
+
+export default config;
